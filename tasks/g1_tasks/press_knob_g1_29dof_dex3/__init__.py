8316053e5c7f80d507d7a3d4bea4f93beffc5f8c
MCxiaoguu <hanyanggu05@gmail.com>
2025-11-12 15:03:55 -0800

    adapted mimic cfg; haven't tested

--- tasks/g1_tasks/press_knob_g1_29dof_dex3/__init__.py
+++ tasks/g1_tasks/press_knob_g1_29dof_dex3/__init__.py
@@ -5,7 +5,7 @@
 import gymnasium as gym
 import os
 
-from . import press_knob_g1_29dof_dex3_joint_env_cfg
+from . import press_knob_g1_29dof_dex3_joint_env_cfg, press_knob_g1_29dof_dex3_joint_mimic_env_cfg
 
 
 gym.register(
@@ -13,6 +13,16 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     kwargs={
         "env_cfg_entry_point": press_knob_g1_29dof_dex3_joint_env_cfg.PressKnobG129DEX3JointEnvCfg,
+    },
+    disable_env_checker=True,
+)
+
+
+gym.register(
+    id="Isaac-Press-Knob-G129-Dex3-Joint-Mimic",
+    entry_point="tasks.g1_tasks.press_knob_g1_29dof_dex3.press_knob_g1_29dof_dex3_joint_mimic_env:PressKnobG129DEX3JointMimicEnv",
+    kwargs={
+        "env_cfg_entry_point": press_knob_g1_29dof_dex3_joint_mimic_env_cfg.PressKnobG129DEX3JointMimicEnvCfg,
     },
     disable_env_checker=True,
 )
